aa1edecca3280c874e092e1d894d7e0661fdfb70
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-02 21:01:40 +0200

    Mhe

--- Weekly1Task2/Weekly1Task2.cpp
+++ Weekly1Task2/Weekly1Task2.cpp
@@ -5,12 +5,25 @@
 
 int name;
 int age;
+//int yes;
+//int no;
 
 int main()
 {
     std::cout << "What is your name? ";
-    std::cin >> name;
+    std::getline (std::cin, name);
+    std::cout << "How old are you? ";
+    std::cin >> age;
+    /*std::cout << "Do you drink coffee?";
+    if yes{
+        std::cout << "Seems you like coffe"
+        }
+    if no{
+        std::cout << "Oh you don`t like coffee"
+    }*/
 
     std::cout << "Your name is: " << name << std::endl;
+    std::cout << "You are" << age << "years old" << std::endl
+
 }
 
