4341daf505d83242d19731277578a782cd2c08c0
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-02 20:48:08 +0200

    Add project files.

---
+++ Weekly1Task2/Weekly1Task2.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+#include <string>
+
+int name;
+int age;
+
+int main()
+{
+    std::cout << "What is your name? ";
+    std::cin >> name;
+
+    std::cout << "Your name is: " << name << std::endl;
+}
+
