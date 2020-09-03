a2652c1a16eb35ac052ce4a31723ddda3ae51c78
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-03 18:49:19 +0200

    Coffee not work

--- Weekly1Task2/Weekly1Task2.cpp
+++ Weekly1Task2/Weekly1Task2.cpp
@@ -3,27 +3,45 @@
 #include <iostream>
 #include <string>
 
-int name;
+#define MAX_LENGTH 100
+
+//int name;
 int age;
-//int yes;
-//int no;
+
+int drink;
 
 int main()
 {
+    char name[MAX_LENGTH] = { 0 };
+
     std::cout << "What is your name? ";
-    std::getline (std::cin, name);
+    //std::getline (std::cin, name);
+    std::cin.getline(name, MAX_LENGTH);
     std::cout << "How old are you? ";
     std::cin >> age;
-    /*std::cout << "Do you drink coffee?";
-    if yes{
-        std::cout << "Seems you like coffe"
+   // std::cout << "Do you drink coffee? Write 1 if yes, 0 if no ";
+    std::cout << "Do you drink coffee? "
+    std::cin >> drink;
+    /*std::cin >> yes;
+    std::cin >> no;
+    if (yes){
+        std::cout << "Seems you like coffe";
         }
-    if no{
-        std::cout << "Oh you don`t like coffee"
+    if (no){
+        std::cout << "Oh you don`t like coffee";
     }*/
+    
+    std::cout << "Your name is " << name << std::endl;
+    std::cout << "You are " << age << " years old" << std::endl;
+        if (drink <= 1) {
+            std::cout << "Seems you like coffee";
+        }
+        if (drink > 1) {
+            std::cout << "Oh you don`t like coffee";
+        }
 
-    std::cout << "Your name is: " << name << std::endl;
-    std::cout << "You are" << age << "years old" << std::endl
+        
+    //std::cout << "Coffee " << drink << std::endl;
 
 }
 
