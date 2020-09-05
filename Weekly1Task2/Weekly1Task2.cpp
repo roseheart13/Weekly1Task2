3a3d60590841c9884aef713069ebafc8997f8375
Rutha <70570030+roseheart13@users.noreply.github.com>
2020-09-05 16:17:22 +0200

    yes

--- Weekly1Task2/Weekly1Task2.cpp
+++ Weekly1Task2/Weekly1Task2.cpp
@@ -2,46 +2,42 @@
 
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 #define MAX_LENGTH 100
 
-//int name;
+
 int age;
 
-int drink;
+
+std::string YN = "";
 
 int main()
 {
     char name[MAX_LENGTH] = { 0 };
 
     std::cout << "What is your name? ";
-    //std::getline (std::cin, name);
     std::cin.getline(name, MAX_LENGTH);
+
     std::cout << "How old are you? ";
     std::cin >> age;
-   // std::cout << "Do you drink coffee? Write 1 if yes, 0 if no ";
-    std::cout << "Do you drink coffee? "
-    std::cin >> drink;
-    /*std::cin >> yes;
-    std::cin >> no;
-    if (yes){
-        std::cout << "Seems you like coffe";
-        }
-    if (no){
-        std::cout << "Oh you don`t like coffee";
-    }*/
+
+    std::cout << "Do you drink coffee? [Eg: Y/N] ";
+    std::cin >> YN;
+ 
     
     std::cout << "Your name is " << name << std::endl;
     std::cout << "You are " << age << " years old" << std::endl;
-        if (drink <= 1) {
+    
+        if (YN == "Y" ) {
             std::cout << "Seems you like coffee";
         }
-        if (drink > 1) {
+        else  {
             std::cout << "Oh you don`t like coffee";
         }
 
         
-    //std::cout << "Coffee " << drink << std::endl;
+   
 
 }
 
